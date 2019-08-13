6fc5dfc842cf109c1adf73d4ae910728bdd1d9c6
Bkah0826 <baldwinkah@gmail.com>
2019-08-12 21:17:19 -0600

    Added Test Project with data source

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AlfrescoTestApps
  
+How to run
+
+1. Open up Command Line
+2. change directory to root folder of project using "cd"
+3. type in "npm start"
--- Test1/proxy.conf.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-    "/alfresco": {
-        "target": "https://yfz44e.trial.alfresco.com",
-        "secure": true,
-        "changeOrigin": true
-    }
-};
